18a323cdd31d98f02db6b7970559d1dc94b3ea0f
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-02-17 01:38:09 +0800

    show/hide pwd

--- src/managed/Form/Account/BackendRegistration.jsx
+++ src/managed/Form/Account/BackendRegistration.jsx
@@ -3,6 +3,8 @@ import { FormGroup, Form, InputGroup } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import PageTitle from "../../../shared/Title";
 import BtnBootstrap from "../../../shared/BtnBootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./Registration.module.scss";
 import zxcvbn from "zxcvbn";
@@ -16,11 +18,15 @@ export default function BackendRegistration() {
     password: "",
     confirmPassword: "",
   });
+
   const [passwordError, setPasswordErr] = useState({
     pwdError:
       "您的密碼長度至少要8-20字元以上，包含數字、英文組合。但不能包含空白或表情符號",
     confirmPwdError: "",
   });
+
+  const [showPwd, setshowPwd] = useState(false);
+  const [showConfirmPwd, setshowConfirmPwd] = useState(false);
 
   const handlInputChange = (e) => {
     setInput({
@@ -81,13 +87,23 @@ export default function BackendRegistration() {
       setPasswordErr({
         ...passwordError,
         confirmPwdError: "輸入密碼與再次確認密碼不符，請重新嘗試",
-      })
-    }else{
+      });
+    } else {
       setPasswordErr({
         ...passwordError,
         confirmPwdError: "",
-      })
+      });
     }
+  };
+
+  const showHidePwd = (e) => {
+    e.preventDefault();
+    setshowPwd(!showPwd);
+  };
+
+  const showHideConfirmPwd = (e) => {
+    e.preventDefault();
+    setshowConfirmPwd(!showConfirmPwd);
   };
 
   return (
@@ -106,7 +122,9 @@ export default function BackendRegistration() {
                 className="mb-3"
                 controlId="exampleForm.ControlInput1"
               >
-                <Form.Label>請輸入Email：</Form.Label>
+                <Form.Label style={{ cursor: "pointer" }}>
+                  請輸入Email：
+                </Form.Label>
                 <Form.Control
                   type="email"
                   name="email"
@@ -115,35 +133,66 @@ export default function BackendRegistration() {
                   value={input.email}
                 />
               </Form.Group>
-              <FormGroup>
-                <Form.Label htmlFor="inputPassword">請輸入密碼：</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  id="inputPassword"
-                  maxLength={20}
-                  aria-describedby="passwordHelpBlock"
-                  onChange={handlInputChange}
-                  onInput={passwordStrength}
-                  value={input.password}
-                />
+
+              <Form.Group className="mb-4">
+                <Form.Label
+                  htmlFor="inputPassword"
+                  style={{ cursor: "pointer" }}
+                >
+                  請輸入密碼：
+                </Form.Label>
+                <InputGroup>
+                  <Form.Control
+                    type={showPwd ? "Text" : "Password"}
+                    name="password"
+                    id="inputPassword"
+                    maxLength={20}
+                    aria-describedby="passwordHelpBlock"
+                    onChange={handlInputChange}
+                    onInput={passwordStrength}
+                    value={input.password}
+                  />
+                  <span className="input-group-text">
+                    <FontAwesomeIcon
+                      id="showPass"
+                      style={{ cursor: "pointer" }}
+                      onClick={showHidePwd}
+                      icon={showPwd ? faEyeSlash : faEye}
+                    />
+                  </span>
+                </InputGroup>
                 <Form.Text id="passwordHelpBlock" muted>
-                  <strong>{passwordError.pwdError}</strong>
+                  {/* <strong>{passwordError.pwdError}</strong> */}
                 </Form.Text>
-                <br />
-                <Form.Label htmlFor="checkInputPassword">
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label
+                  htmlFor="checkInputPassword"
+                  style={{ cursor: "pointer" }}
+                >
                   請確認輸入密碼：
                 </Form.Label>
-                <Form.Control
-                  type="password"
-                  name="confirmPassword"
-                  id="checkInputPassword"
-                  onChange={handlInputChange}
-                  onBlur={validatePwd}
-                  value={input.confirmPassword}
-                  aria-describedby="passwordHelpBlock"
-                />
-              </FormGroup>
+                <InputGroup>
+                  <Form.Control
+                    type={showConfirmPwd ? "Text" : "Password"}
+                    name="confirmPassword"
+                    id="checkInputPassword"
+                    onChange={handlInputChange}
+                    onBlur={validatePwd}
+                    value={input.confirmPassword}
+                    aria-describedby="passwordHelpBlock"
+                  />
+                  <span className="input-group-text">
+                    <FontAwesomeIcon
+                      id="showConfirmPass"
+                      style={{ cursor: "pointer" }}
+                      onClick={showHideConfirmPwd}
+                      icon={showConfirmPwd ? faEyeSlash : faEye}
+                    />
+                  </span>
+                </InputGroup>
+              </Form.Group>
               <div className="d-grid gap-2 mt-5">
                 <btn.PrimaryBtn text={"送出"} />
               </div>
