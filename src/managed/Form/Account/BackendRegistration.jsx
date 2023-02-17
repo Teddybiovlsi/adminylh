a37da542c7999a7db3e8161dca261a2580e259a2
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-02-17 23:15:12 +0800

    StrengthPwd

--- src/managed/Form/Account/BackendRegistration.jsx
+++ src/managed/Form/Account/BackendRegistration.jsx
@@ -1,32 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { FormGroup, Form, InputGroup } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, InputGroup } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import PageTitle from "../../../shared/Title";
 import BtnBootstrap from "../../../shared/BtnBootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import "bootstrap/dist/css/bootstrap.min.css";
+import useBoolean from "../shared/useBoolean";
+import PwdStrengthMeter from "../shared/PwdStrengthMeter";
 import styles from "./Registration.module.scss";
 import zxcvbn from "zxcvbn";
-import { func } from "prop-types";
 
 export default function BackendRegistration() {
   const btn = new BtnBootstrap();
-
+  const pwdHint =
+    "您的密碼長度至少要8-20字元以上，包含數字、英文組合。但不能包含空白";
+  const checkPwdHint = "請再次輸入您的密碼";
   const [input, setInput] = useState({
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-  const [passwordError, setPasswordErr] = useState({
-    pwdError:
-      "您的密碼長度至少要8-20字元以上，包含數字、英文組合。但不能包含空白或表情符號",
-    confirmPwdError: "",
-  });
+  const [confirmPwdError, setConfirmPwdError] = useState("");
+  const [pwdScore, setPwdScore] = useState(0);
+  const [showPwd, { setShowPwd }] = useBoolean(false);
 
-  const [showPwd, setshowPwd] = useState(false);
-  const [showConfirmPwd, setshowConfirmPwd] = useState(false);
+  // useEffect(() => {
+  //   switch (pwdScore) {
+  //     case 0:
+  //       setPasswordErr({
+  //         ...passwordError,
+  //         pwdError: "密碼強度為：極弱 無法通過註冊",
+  //       });
+  //       break;
+  //     case 1:
+  //       setPasswordErr({ ...passwordError, pwdError: "密碼強度為：偏弱" });
+  //       break;
+  //     case 2:
+  //       setPasswordErr({ ...passwordError, pwdError: "密碼強度為：普通" });
+  //       break;
+  //     case 3:
+  //       setPasswordErr({ ...passwordError, pwdError: "密碼強度為：強" });
+  //       break;
+  //     case 4:
+  //       setPasswordErr({ ...passwordError, pwdError: "密碼強度為：最強" });
+  //       break;
+  //     default:
+  //       setPasswordErr({ ...passwordError });
+  //   }
+  // }, [pwdScore]);
 
   const handlInputChange = (e) => {
     setInput({
@@ -36,50 +58,28 @@ export default function BackendRegistration() {
   };
 
   const passwordStrength = (e) => {
-    let pwdScore = 0;
-
     const passwordValue = e.target.value;
     const whitespaceRegExp = /^$|\s+/;
     const whiteSpace = whitespaceRegExp.test(passwordValue);
 
     if (passwordValue === "") {
-      setPasswordErr({
-        ...passwordError,
-        pwdError: "密碼欄位不得為空",
-      });
+      // setPasswordErr({
+      //   ...passwordError,
+      //   pwdError: "密碼欄位不得為空",
+      // });
     } else {
       if (whiteSpace) {
-        setPasswordErr({
-          ...passwordError,
-          pwdError: "密碼欄位不得有空白鍵",
-        });
+        // setPasswordErr({
+        //   ...passwordError,
+        //   pwdError: "密碼欄位不得有空白鍵",
+        // });
       } else {
-        setPasswordErr({
-          ...passwordError,
-          pwdError: "",
-        });
         const pw = zxcvbn(passwordValue);
-        pwdScore = pw.score;
-        switch (pwdScore) {
-          case 1:
-            setPasswordErr({ ...passwordError, pwdError: "密碼強度為：極弱" });
-            break;
-          case 2:
-            setPasswordErr({ ...passwordError, pwdError: "密碼強度為：偏弱" });
-            break;
-          case 3:
-            setPasswordErr({ ...passwordError, pwdError: "密碼強度為：普通" });
-            break;
-          case 4:
-            setPasswordErr({ ...passwordError, pwdError: "密碼強度為：強" });
-            break;
-          case 5:
-            setPasswordErr({ ...passwordError, pwdError: "密碼強度為：極強" });
-            break;
-        }
+        setPwdScore(pw.score);
       }
     }
   };
+  // 驗證密碼
   const validatePwd = (e) => {
     if (!e.target.value) {
       setPasswordErr({ ...passwordError, confirmPwdError: "請再次輸入密碼" });
@@ -94,16 +94,6 @@ export default function BackendRegistration() {
         confirmPwdError: "",
       });
     }
-  };
-
-  const showHidePwd = (e) => {
-    e.preventDefault();
-    setshowPwd(!showPwd);
-  };
-
-  const showHideConfirmPwd = (e) => {
-    e.preventDefault();
-    setshowConfirmPwd(!showConfirmPwd);
   };
 
   return (
@@ -126,15 +116,17 @@ export default function BackendRegistration() {
                   請輸入Email：
                 </Form.Label>
                 <Form.Control
+                  required
                   type="email"
                   name="email"
                   placeholder="name@example.com"
                   onChange={handlInputChange}
                   value={input.email}
                 />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group className="mb-4">
+              <Form.Group className="mb-1">
                 <Form.Label
                   htmlFor="inputPassword"
                   style={{ cursor: "pointer" }}
@@ -148,23 +140,23 @@ export default function BackendRegistration() {
                     id="inputPassword"
                     maxLength={20}
                     aria-describedby="passwordHelpBlock"
+                    placeholder={pwdHint}
                     onChange={handlInputChange}
                     onInput={passwordStrength}
                     value={input.password}
+                    required
                   />
                   <span className="input-group-text">
                     <FontAwesomeIcon
                       id="showPass"
                       style={{ cursor: "pointer" }}
-                      onClick={showHidePwd}
+                      onClick={setShowPwd}
                       icon={showPwd ? faEyeSlash : faEye}
                     />
                   </span>
                 </InputGroup>
-                <Form.Text id="passwordHelpBlock" muted>
-                  {/* <strong>{passwordError.pwdError}</strong> */}
-                </Form.Text>
               </Form.Group>
+              <PwdStrengthMeter pwdScore={pwdScore} />
 
               <Form.Group>
                 <Form.Label
@@ -175,26 +167,28 @@ export default function BackendRegistration() {
                 </Form.Label>
                 <InputGroup>
                   <Form.Control
-                    type={showConfirmPwd ? "Text" : "Password"}
+                    type={showPwd ? "Text" : "Password"}
                     name="confirmPassword"
                     id="checkInputPassword"
                     onChange={handlInputChange}
                     onBlur={validatePwd}
                     value={input.confirmPassword}
+                    placeholder={checkPwdHint}
                     aria-describedby="passwordHelpBlock"
+                    required
                   />
                   <span className="input-group-text">
                     <FontAwesomeIcon
                       id="showConfirmPass"
                       style={{ cursor: "pointer" }}
-                      onClick={showHideConfirmPwd}
-                      icon={showConfirmPwd ? faEyeSlash : faEye}
+                      onClick={setShowPwd}
+                      icon={showPwd ? faEyeSlash : faEye}
                     />
                   </span>
                 </InputGroup>
               </Form.Group>
               <div className="d-grid gap-2 mt-5">
-                <btn.PrimaryBtn text={"送出"} />
+                <btn.PrimaryBtn text={"送出"} type={"submit"} />
               </div>
             </Form>
           </Card.Body>
