4d36c1a637ea8bb9cf338cdfc4566001d5cbbfab
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-02-18 02:00:28 +0800

    email-vailidation v1

--- src/managed/Form/Account/BackendRegistration.jsx
+++ src/managed/Form/Account/BackendRegistration.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { Formik } from "formik";
+import * as yup from "yup";
 import { Form, InputGroup } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import Feedback from "react-bootstrap/Feedback";
 import PageTitle from "../../../shared/Title";
 import BtnBootstrap from "../../../shared/BtnBootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,7 +14,12 @@ import styles from "./Registration.module.scss";
 import zxcvbn from "zxcvbn";
 
 export default function BackendRegistration() {
+  // 實例化btn元件
   const btn = new BtnBootstrap();
+  //email規則
+  const emailRule =
+    /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+
   const pwdHint =
     "您的密碼長度至少要8-20字元以上，包含數字、英文組合。但不能包含空白";
   const checkPwdHint = "請再次輸入您的密碼";
@@ -25,30 +33,19 @@ export default function BackendRegistration() {
   const [pwdScore, setPwdScore] = useState(0);
   const [showPwd, { setShowPwd }] = useBoolean(false);
 
-  // useEffect(() => {
-  //   switch (pwdScore) {
-  //     case 0:
-  //       setPasswordErr({
-  //         ...passwordError,
-  //         pwdError: "密碼強度為：極弱 無法通過註冊",
-  //       });
-  //       break;
-  //     case 1:
-  //       setPasswordErr({ ...passwordError, pwdError: "密碼強度為：偏弱" });
-  //       break;
-  //     case 2:
-  //       setPasswordErr({ ...passwordError, pwdError: "密碼強度為：普通" });
-  //       break;
-  //     case 3:
-  //       setPasswordErr({ ...passwordError, pwdError: "密碼強度為：強" });
-  //       break;
-  //     case 4:
-  //       setPasswordErr({ ...passwordError, pwdError: "密碼強度為：最強" });
-  //       break;
-  //     default:
-  //       setPasswordErr({ ...passwordError });
+  // const validate = (values) => {
+  //   let errors = {};
+  //   if (!values.email) {
+  //     errors.email = "Required";
+  //   } else if (!emailRule.test(values)) {
+  //     errors.email = "請輸入合法的email名稱";
   //   }
-  // }, [pwdScore]);
+  //   return errors;
+  // };
+
+  const schema = yup.object({
+    email: yup.string().email("請輸入合法的信箱").required("信箱欄位不得為空"),
+  });
 
   const handlInputChange = (e) => {
     setInput({
@@ -57,45 +54,47 @@ export default function BackendRegistration() {
     });
   };
 
-  const passwordStrength = (e) => {
-    const passwordValue = e.target.value;
-    const whitespaceRegExp = /^$|\s+/;
-    const whiteSpace = whitespaceRegExp.test(passwordValue);
+  // const passwordStrength = (e) => {
+  //   const passwordValue = e.target.value;
+  //   const whitespaceRegExp = /^$|\s+/;
+  //   const whiteSpace = whitespaceRegExp.test(passwordValue);
 
-    if (passwordValue === "") {
-      // setPasswordErr({
-      //   ...passwordError,
-      //   pwdError: "密碼欄位不得為空",
-      // });
-    } else {
-      if (whiteSpace) {
-        // setPasswordErr({
-        //   ...passwordError,
-        //   pwdError: "密碼欄位不得有空白鍵",
-        // });
-      } else {
-        const pw = zxcvbn(passwordValue);
-        setPwdScore(pw.score);
-      }
-    }
-  };
+  //   if (passwordValue === "") {
+  //     // setPasswordErr({
+  //     //   ...passwordError,
+  //     //   pwdError: "密碼欄位不得為空",
+  //     // });
+  //   } else {
+  //     if (whiteSpace) {
+  //       // setPasswordErr({
+  //       //   ...passwordError,
+  //       //   pwdError: "密碼欄位不得有空白鍵",
+  //       // });
+  //     } else {
+  //       const pw = zxcvbn(passwordValue);
+  //       setPwdScore(pw.score);
+  //     }
+  //   }
+  // };
+
   // 驗證密碼
-  const validatePwd = (e) => {
-    if (!e.target.value) {
-      setPasswordErr({ ...passwordError, confirmPwdError: "請再次輸入密碼" });
-    } else if (input.password && e.target.value !== input.password) {
-      setPasswordErr({
-        ...passwordError,
-        confirmPwdError: "輸入密碼與再次確認密碼不符，請重新嘗試",
-      });
-    } else {
-      setPasswordErr({
-        ...passwordError,
-        confirmPwdError: "",
-      });
-    }
-  };
+  // const validatePwd = (e) => {
+  //   if (!e.target.value) {
+  //     setPasswordErr({ ...passwordError, confirmPwdError: "請再次輸入密碼" });
+  //   } else if (input.password && e.target.value !== input.password) {
+  //     setPasswordErr({
+  //       ...passwordError,
+  //       confirmPwdError: "輸入密碼與再次確認密碼不符，請重新嘗試",
+  //     });
+  //   } else {
+  //     setPasswordErr({
+  //       ...passwordError,
+  //       confirmPwdError: "",
+  //     });
+  //   }
+  // };
 
+  // const showEmailError = formik.touched.email && formik.errors.email;
   return (
     <>
       <PageTitle title="台大分院雲林分院｜創建後台使用者" />
@@ -107,33 +106,52 @@ export default function BackendRegistration() {
             </h1>
           </Card.Title>
           <Card.Body>
-            <Form>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label style={{ cursor: "pointer" }}>
-                  請輸入Email：
-                </Form.Label>
-                <Form.Control
-                  required
-                  type="email"
-                  name="email"
-                  placeholder="name@example.com"
-                  onChange={handlInputChange}
-                  value={input.email}
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
+            <Formik
+              validationSchema={schema}
+              onSubmit={console.log}
+              initialValues={{
+                email: "",
+              }}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                handleBlur,
+                values,
+                errors,
+                isValid,
+                isSubmitting,
+              }) => (
+                <Form noValidate onSubmit={handleSubmit}>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label style={{ cursor: "pointer" }}>
+                      請輸入Email：
+                    </Form.Label>
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      placeholder="name@example.com"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      isInvalid={!!errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.email}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-              <Form.Group className="mb-1">
+                  {/* <Form.Group className="mb-1">
                 <Form.Label
                   htmlFor="inputPassword"
                   style={{ cursor: "pointer" }}
                 >
                   請輸入密碼：
                 </Form.Label>
-                <InputGroup>
+                <InputGroup className="mb-1">
                   <Form.Control
                     type={showPwd ? "Text" : "Password"}
                     name="password"
@@ -155,8 +173,9 @@ export default function BackendRegistration() {
                     />
                   </span>
                 </InputGroup>
+                <PwdStrengthMeter pwdScore={pwdScore} />
               </Form.Group>
-              <PwdStrengthMeter pwdScore={pwdScore} />
+              
 
               <Form.Group>
                 <Form.Label
@@ -186,11 +205,13 @@ export default function BackendRegistration() {
                     />
                   </span>
                 </InputGroup>
-              </Form.Group>
-              <div className="d-grid gap-2 mt-5">
-                <btn.PrimaryBtn text={"送出"} type={"submit"} />
-              </div>
-            </Form>
+              </Form.Group> */}
+                  <div className="d-grid gap-2 mt-5">
+                    <btn.PrimaryBtn text={"送出"} btnType={"submit"} />
+                  </div>
+                </Form>
+              )}
+            </Formik>
           </Card.Body>
         </Card>
       </div>
