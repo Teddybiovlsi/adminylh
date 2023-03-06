896ddc437a178b339e0ba3dcc50dd9ac5491d23c
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-03-06 18:23:35 +0800

    實作中

--- src/managed/Form/shared/InputFormPreviewFunction.jsx
+++ src/managed/Form/shared/InputFormPreviewFunction.jsx
@@ -4,15 +4,16 @@ import { Form, Card } from "react-bootstrap";
 import CardTitleFunction from "./CardTitleFunction";
 import BtnBootstrap from "../../../shared/BtnBootstrap";
 import styles from "./scss/FormStyles.module.scss";
-function InputFormPreviewFunction(
+function InputFormPreviewFunction({
   FormMode = false,
-  VidoeName = "",
-  VideoLanguage = "",
-  VideoQA,
+  VideoName = "",
+  VideoLanguage="",
   GoPrevEvent = null,
-  GoNextEvent = null
-) {
+  GoNextEvent = null,
+}) {
+  // 將製作的元件庫 button實例化
   const btn = new BtnBootstrap();
+
   return (
     <div className="FormStyle d-flex align-items-center justify-content-center">
       <PageTitle
@@ -26,6 +27,13 @@ function InputFormPreviewFunction(
             }表單系統`}
           />
         </Card.Title>
+        <Card.Body>
+          <Card.Title>影片名稱：</Card.Title>
+          <Card.Text>{VideoName}</Card.Text>
+          <Card.Title>影片語言：</Card.Title>
+          <Card.Text>{VideoLanguage}</Card.Text>
+
+        </Card.Body>
         <Card.Footer>
           <btn.PrimaryBtn
             btnName={"formStep"}
