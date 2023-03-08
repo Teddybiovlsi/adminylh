0ca76911970980f64caa697197666cdd8551f4c4
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-03-08 18:36:21 +0800

    V1.0.0

--- src/shared/Title.jsx
+++ src/shared/Title.jsx
@@ -1,3 +1,6 @@
+// 這個套件主要使用於分頁上的標題文字
+// 傳入的參數為：Title
+// Title內不限任何文字皆可以進行填入
 import React from "react";
 import PropTypes from "prop-types";
 import { Helmet, HelmetProvider } from "react-helmet-async";
