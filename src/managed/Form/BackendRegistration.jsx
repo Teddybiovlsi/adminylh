18fba4c1a9834ef3bf4186b6e981fd7d44011b94
Teddybiovlsi <M11113005@yuntech.edu.tw>
2023-04-10 18:08:19 +0800

    轉址完成/新增伺服器狀態碼方法

--- src/managed/Form/BackendRegistration.jsx
+++ src/managed/Form/BackendRegistration.jsx
@@ -1,7 +1,7 @@
 // 建立後台使用者元件
 // 包含email認證/密碼驗證/2次密碼驗證
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { Formik } from 'formik';
 import * as yup from 'yup';
@@ -16,7 +16,6 @@ import zxcvbn from 'zxcvbn';
 import { post } from '../axios';
 import styles from '../../styles/Form/Registration.module.scss';
 import { useNavigate } from 'react-router-dom';
-import { redirect } from 'react-router-dom';
 
 export default function BackendRegistration() {
   const checkPwdHint = '請再次輸入您的密碼';
@@ -33,23 +32,41 @@ export default function BackendRegistration() {
 
   const [shouldRedirect, setShouldRedirect] = React.useState(false);
 
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (shouldRedirect) {
+      return navigate('/');
+    }
+  }, [shouldRedirect]);
+
   // create a async function to send data to backend
   const sendBackendRegistrationData = async (data) => {
     try {
-      const response = await post('/api/user/register', data);
-      // console.log(response);
-      // console.log(response.data);
-      // console.log(response.data.data);
-      // console.log(response.data.data.message);
-      setSuccessMessage(response.data.data.message);
+      // 正確格式API
+      const response = await post('admin', data);
+      setSuccessMessage('成功創建後台使用者');
       setSuccessBoolean(true);
       setShouldRedirect(true);
     } catch (error) {
-      // console.log(error);
-      // console.log(error.response);
-      // console.log(error.response.data);
-      // console.log(error.response.data.message);
-      setErrorMessage(error.response.data.message);
+      // if status code is 400, then it is a bad request
+      switch (error.response.status) {
+        case 303:
+          setErrorMessage('信箱已被註冊');
+          break;
+        case 400:
+          setErrorMessage('格式錯誤');
+          break;
+        case 404:
+          setErrorMessage('找不到請求之資源');
+          break;
+        case 500:
+          setErrorMessage('伺服器錯誤');
+          break;
+        default:
+          setErrorMessage('未知錯誤');
+          break;
+      }
     }
   };
 
@@ -66,117 +83,116 @@ export default function BackendRegistration() {
       .required('再次確認密碼欄位不得為空!')
       .oneOf([yup.ref('password'), null], '前後密碼必須一致'),
   });
-  if (shouldRedirect) {
-    return <redirect to='/' />;
-  } else {
-    return (
-      <>
-        <PageTitle title='台大分院雲林分院｜創建後台使用者' />
-        {successMessage || errorMessage ? (
-          <AlertBootstrap
-            ifsucceed={successBoolean}
-            variant={successMessage ? 'success' : 'danger'}
-            children={successMessage ? successMessage : errorMessage}
-          />
-        ) : (
-          ''
-        )}
-        <div className='FormStyle d-flex align-items-center justify-content-center'>
-          <Card className={`${styles.RegisterCard}`}>
-            <Card.Title className={`${styles.FormTitle}`}>
-              <h1 className='fs-2'>
-                <strong>創建後台使用者</strong>
-              </h1>
-            </Card.Title>
-            <Card.Body>
-              <Formik
-                validationSchema={schema}
-                onSubmit={(data, { resetForm }) => {
-                  //call sendBackendRegistrationData function and check if it is successful
-                  try {
-                    sendBackendRegistrationData(data);
-                    // reset form after submit
-                    resetForm();
-                    // redirect to login page after submit successfully 5s
-                    setTimeout(() => {
-                      setShouldRedirect(true);
-                    }, 5000);
-                  } catch (error) {
-                    console.log(error);
-                  }
-                }}
-                initialValues={{
-                  email: '',
-                  password: '',
-                  confirmPassword: '',
-                }}
-              >
-                {({
-                  handleSubmit,
-                  handleChange,
-                  handleBlur,
-                  values,
-                  errors,
-                  touched,
-                }) => (
-                  <Form noValidate onSubmit={handleSubmit}>
-                    <FormEmail
-                      ChangeEvent={handleChange}
-                      BlurEvent={handleBlur}
-                      EmailValue={values.email}
-                      ValidCheck={touched.email && !errors.email}
-                      InValidCheck={!!errors.email}
-                      ErrorMessage={errors.email}
+
+  return (
+    <>
+      <PageTitle title='台大分院雲林分院｜創建後台使用者' />
+      {successMessage || errorMessage ? (
+        <AlertBootstrap
+          ifsucceed={successBoolean}
+          variant={successMessage ? 'success' : 'danger'}
+          children={successMessage ? successMessage : errorMessage}
+        />
+      ) : (
+        ''
+      )}
+      <div className='FormStyle d-flex align-items-center justify-content-center'>
+        <Card className={`${styles.RegisterCard}`}>
+          <Card.Title className={`${styles.FormTitle}`}>
+            <h1 className='fs-2'>
+              <strong>創建後台使用者</strong>
+            </h1>
+          </Card.Title>
+          <Card.Body>
+            <Formik
+              validationSchema={schema}
+              onSubmit={(data, { resetForm }) => {
+                //call sendBackendRegistrationData function and check if it is successful
+                sendBackendRegistrationData(data);
+
+                if (successMessage) {
+                  resetForm();
+                  // redirect to Home page after 5 seconds
+                  setTimeout(() => {
+                    setShouldRedirect(true);
+                  }, 5000);
+                } else if (errorMessage) {
+                  console.log('error');
+                } else {
+                  console.log('error');
+                }
+              }}
+              initialValues={{
+                email: '',
+                password: '',
+                confirmPassword: '',
+              }}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                handleBlur,
+                values,
+                errors,
+                touched,
+              }) => (
+                <Form noValidate onSubmit={handleSubmit}>
+                  <FormEmail
+                    ChangeEvent={handleChange}
+                    BlurEvent={handleBlur}
+                    EmailValue={values.email}
+                    ValidCheck={touched.email && !errors.email}
+                    InValidCheck={!!errors.email}
+                    ErrorMessage={errors.email}
+                  />
+                  <FormPwd
+                    GroupClassName='mb-1'
+                    SetStrengthMeter={true}
+                    StrengthMeterPwdScore={pwdScore}
+                    ChangeEvent={handleChange}
+                    BlurEvent={handleBlur}
+                    InputEvent={(e) => {
+                      setPwdScore(zxcvbn(e.target.value).score);
+                    }}
+                    PwdValue={values.password}
+                    ValidCheck={touched.password & !errors.password}
+                    InValidCheck={!!errors.password}
+                    ControlID={'inputPassword'}
+                    IconID={'showPass'}
+                    SetShowPwdCondition={setShowPwd}
+                    ShowPwdCondition={showPwd}
+                    ErrorMessage={errors.password}
+                  />
+                  <FormPwd
+                    LabelForName='checkInputPassword'
+                    ControlName='confirmPassword'
+                    LabelMessage='請再次確認輸入密碼'
+                    FormControlPlaceHolder={checkPwdHint}
+                    BlurEvent={handleBlur}
+                    ChangeEvent={handleChange}
+                    PwdValue={values.confirmPassword}
+                    SetShowPwdCondition={setShowPwd}
+                    ShowPwdCondition={showPwd}
+                    ValidCheck={
+                      touched.confirmPassword & !errors.confirmPassword
+                    }
+                    InValidCheck={!!errors.confirmPassword}
+                    CorrectMessage='確認密碼與輸入密碼相符'
+                    ErrorMessage={errors.confirmPassword}
+                  />
+                  <div className={`${styles.btnPosition} d-grid gap-2 p-2`}>
+                    <BtnBootstrap
+                      text={'送出'}
+                      btnType={'submit'}
+                      variant={'primary'}
                     />
-                    <FormPwd
-                      GroupClassName='mb-1'
-                      SetStrengthMeter={true}
-                      StrengthMeterPwdScore={pwdScore}
-                      ChangeEvent={handleChange}
-                      BlurEvent={handleBlur}
-                      InputEvent={(e) => {
-                        setPwdScore(zxcvbn(e.target.value).score);
-                      }}
-                      PwdValue={values.password}
-                      ValidCheck={touched.password & !errors.password}
-                      InValidCheck={!!errors.password}
-                      ControlID={'inputPassword'}
-                      IconID={'showPass'}
-                      SetShowPwdCondition={setShowPwd}
-                      ShowPwdCondition={showPwd}
-                      ErrorMessage={errors.password}
-                    />
-                    <FormPwd
-                      LabelForName='checkInputPassword'
-                      ControlName='confirmPassword'
-                      LabelMessage='請再次確認輸入密碼'
-                      FormControlPlaceHolder={checkPwdHint}
-                      BlurEvent={handleBlur}
-                      ChangeEvent={handleChange}
-                      PwdValue={values.confirmPassword}
-                      SetShowPwdCondition={setShowPwd}
-                      ShowPwdCondition={showPwd}
-                      ValidCheck={
-                        touched.confirmPassword & !errors.confirmPassword
-                      }
-                      InValidCheck={!!errors.confirmPassword}
-                      CorrectMessage='確認密碼與輸入密碼相符'
-                      ErrorMessage={errors.confirmPassword}
-                    />
-                    <div className={`${styles.btnPosition} d-grid gap-2 p-2`}>
-                      <BtnBootstrap
-                        text={'送出'}
-                        btnType={'submit'}
-                        variant={'primary'}
-                      />
-                    </div>
-                  </Form>
-                )}
-              </Formik>
-            </Card.Body>
-          </Card>
-        </div>
-      </>
-    );
-  }
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
 }
---
+++ src/sys/StatusCode.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function StatusCode({ statusCode }) {
+  switch (statusCode) {
+    case 200:
+      return '成功';
+    case 303:
+      return '帳號已被註冊，請重新輸入';
+    case 400:
+      return '格式錯誤';
+    case 401:
+      return '請先登入';
+    case 403:
+      return '您沒有權限進行此操作';
+    case 404:
+      return '找不到請求之資源';
+    case 500:
+      return '伺服器錯誤';
+    default:
+      return '未知錯誤';
+  }
+}
