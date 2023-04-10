4917d7b4c5b5cf262995e4bb12faf45146b9f5d2
Teddybiovlsi <M11113005@yuntech.edu.tw>
2023-04-10 16:57:02 +0800

    修改引用函式

--- src/managed/Pages/Home.jsx
+++ src/managed/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import { Form, Table } from 'react-bootstrap';
-import axios from '../axios';
+import { get, post } from '../axios';
 import styles from '../../styles/pages/HomePage.module.scss';
 import { check } from 'prettier';
 
