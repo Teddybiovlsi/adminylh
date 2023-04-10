3f312963d5b2c3e6398981d29581f60a1accbce8
Teddybiovlsi <M11113005@yuntech.edu.tw>
2023-04-10 16:56:01 +0800

    修正當前版本調用

--- src/managed/axios.jsx
+++ src/managed/axios.jsx
@@ -1,16 +1,24 @@
-// 此為後端API的axios接口，可視情況進行修改
-import _axios from 'axios';
+import _axios from "axios";
 
 const axios = (baseURL) => {
   // 建立自訂義的axios
   const instance = _axios.create({
-    baseURL: baseURL || 'http://140.125.35.82:8079/ntuh-API/public/api', //JSON-Server端口位置
-    headers: { 'Content-Type': 'application/json' },
+    baseURL: baseURL || "http://140.125.35.82:8079/ntuh-API/public/", //JSON-Server端口位置
+    headers: { "Content-Type": "application/json" },
     timeout: 1000,
   });
 
   return instance;
 };
+
+// make axios get request api from the baseURL and /api/v1/GET/ + path
+export const get = (path) => axios().get(`api/v1/GET/${path}`);
+// make axios post request api from the baseURL and /api/v1/POST/ + path
+export const post = (path, data) => axios().post(`api/v1/POST/${path}`, data);
+// make axios put request api from the baseURL and /api/v1/PUT/ + path
+export const put = (path, data) => axios().put(`api/v1/PUT/${path}`, data);
+// make axios delete request api from the baseURL and /api/v1/DELETE/ + path
+export const del = (path) => axios().delete(`api/v1/DELETE/${path}`);
 
 export { axios };
 export default axios();
