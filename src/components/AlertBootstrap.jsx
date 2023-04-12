cd003f85a073e1bbdec0305a44e9a4cb580feff5
Teddybiovlsi <M11113005@yuntech.edu.tw>
2023-04-13 02:01:34 +0800

    晚點實作下數功能

--- src/components/AlertBootstrap.jsx
+++ src/components/AlertBootstrap.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Alert } from "react-bootstrap";
 import BtnBootstrap from "./BtnBootstrap";
 
@@ -8,6 +8,15 @@ export default function AlertBootstrap({
   children,
 }) {
   const [show, setShow] = useState(true);
+
+  const seconds = 5;
+
+  // useEffect(() => {
+    // when ifsucceed is true, then redirect to Home page after 5 seconds
+    // and the second will be counted down
+    // if(seconds >)
+    //   seconds = seconds - 1;
+    // }, [ifsucceed]);
   // if show is false, then the alert will not be rendered
   if (show) {
     return (
@@ -23,7 +32,7 @@ export default function AlertBootstrap({
           {ifsucceed ? "成功！" : "發生了一些錯誤"}{" "}
         </Alert.Heading>
         {children}
-        {ifsucceed ? "5秒後將回到主頁面" : ""}
+        {ifsucceed ? "秒後將回到主頁面" : ""}
       </Alert>
     );
   }
