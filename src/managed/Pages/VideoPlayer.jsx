f88378da66aa9368067903478faa5c0eb58ba9d4
Teddybiovlsi <M11113005@yuntech.edu.tw>
2023-04-17 23:06:41 +0800

    完成一部分API

--- src/managed/Pages/VideoPlayer.jsx
+++ src/managed/Pages/VideoPlayer.jsx
@@ -1,7 +1,33 @@
-import React from "react";
-import VideoJS from "../../components/VideoJS";
+import React, { useEffect, useState } from 'react';
+import VideoJS from '../../components/VideoJS';
+import { useLocation } from 'react-router-dom';
+import StatusCode from '../../sys/StatusCode';
+import { get } from '../axios';
 
-export default function VideoPlayer({ VideoUUID }) {
+export default function VideoPlayer() {
+  const [info, setInfo] = useState({});
+
+  const [error, setError] = useState(null);
+
+  const location = useLocation();
+  const VideoUUID = location.state?.UUID;
+
+  const fetchVideoData = async ({ api }) => {
+    try {
+      const response = await get(api);
+      const data = await response.json();
+      setInfo(data);
+    } catch (error) {
+      setError(StatusCode(error.response.status));
+    }
+  };
+
+  useEffect(() => {
+    fetchVideoData({
+      api: `/videoInfo/${VideoUUID}`,
+    });
+  }, []);
+
   const playerRef = React.useRef(null);
 
   const videoJsOptions = {
@@ -11,8 +37,8 @@ export default function VideoPlayer({ VideoUUID }) {
     fluid: true,
     sources: [
       {
-        src: "/path/to/video.mp4",
-        type: "video/mp4",
+        src: '/path/to/video.mp4',
+        type: 'video/mp4',
       },
     ],
   };
@@ -21,12 +47,12 @@ export default function VideoPlayer({ VideoUUID }) {
     playerRef.current = player;
 
     // You can handle player events here, for example:
-    player.on("waiting", () => {
-      videojs.log("player is waiting");
+    player.on('waiting', () => {
+      VideoJS.log('player is waiting');
     });
 
-    player.on("dispose", () => {
-      videojs.log("player will dispose");
+    player.on('dispose', () => {
+      VideoJS.log('player will dispose');
     });
   };
   return (
