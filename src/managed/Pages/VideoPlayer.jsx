2eb6591551010ae3ed9040c125314896764e502a
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-04-19 18:35:42 +0800

    完成中斷時間

--- src/managed/Pages/VideoPlayer.jsx
+++ src/managed/Pages/VideoPlayer.jsx
@@ -5,6 +5,7 @@ import StatusCode from "../../sys/StatusCode";
 import { get } from "../axios";
 import "video.js/dist/video-js.css";
 import Loading from "../../components/Loading";
+import { forEach, update } from "lodash";
 
 export default function VideoPlayer() {
   const location = useLocation();
@@ -17,12 +18,15 @@ export default function VideoPlayer() {
   const [loading, setLoading] = useState(false);
   const [isFinish, setIsFinish] = useState(false);
 
+  let arrayNum = 0;
+
   const videoJsOptions = {
     controls: true,
     autoplay: false,
     playbackRates: [0.5, 1, 1.25, 1.5, 2],
     responsive: true,
     fluid: true,
+    muted: true,
     sources: [
       {
         src: VideoPath,
@@ -31,68 +35,25 @@ export default function VideoPlayer() {
     ],
   };
 
-  const fetchVideoData = async ({ api }) => {
-    try {
-      setLoading(true);
-      const response = await get(api);
-      const VideoInfo = await response.data.videoQA;
-      VideoInfo.map((item) => {
-        if (!ignore) {
-          setVideoQA((prev) => [
-            ...prev,
-            {
-              video_question: item.video_question,
-              video_duration: item.video_duration,
-              video_interrupt_time: item.video_interrupt_time,
-              option_1: JSON.parse(item.option_1),
-              option_2: JSON.parse(item.option_2),
-              option_3: JSON.parse(item.option_3),
-              option_4: JSON.parse(item.option_4),
-            },
-          ]);
-        }
-      });
-      // setInfo(VideoInfo);
-      setLoading(false);
-    } catch (error) {
-      setError(StatusCode(error.response.status));
-    }
-  };
-
   useEffect(() => {
     let ignore = false;
-
-    async function fetchVideoData({ api }) {
-      try {
-        setLoading(true);
-        const response = await get(api);
-        const VideoInfo = await response.data.videoQA;
-        VideoInfo.map((item) => {
-          if (!ignore) {
-            setVideoQA((prev) => [
-              ...prev,
-              {
-                video_question: item.video_question,
-                video_duration: item.video_duration,
-                video_interrupt_time: item.video_interrupt_time,
-                option_1: JSON.parse(item.option_1),
-                option_2: JSON.parse(item.option_2),
-                option_3: JSON.parse(item.option_3),
-                option_4: JSON.parse(item.option_4),
-              },
-            ]);
-          }
-        });
-        // setInfo(VideoInfo);
-        setLoading(false);
-      } catch (error) {
-        setError(StatusCode(error.response.status));
+    if (!ignore) {
+      async function fetchVideoData({ api }) {
+        try {
+          setLoading(true);
+          const response = await get(api);
+          const VideoInfo = await response.data.data;
+          setInfo(VideoInfo);
+          setLoading(false);
+        } catch (error) {
+          // setError(StatusCode(error.response.status));
+        }
       }
-    }
 
-    fetchVideoData({
-      api: `videoQA/${VideoUUID}`,
-    });
+      fetchVideoData({
+        api: `videoQA/${VideoUUID}`,
+      });
+    }
 
     return () => {
       ignore = true;
@@ -106,11 +67,42 @@ export default function VideoPlayer() {
 
     // You can handle player events here, for example:
     player.on("waiting", () => {
-      videoJS.log("player is waiting");
+      console.log("player is waiting");
     });
 
     player.on("dispose", () => {
-      videoJS.log("player will dispose");
+      console.log("player will dispose");
+    });
+
+    player.on("pause", () => {
+      console.log("player is paused");
+      // show the certain time
+      console.log(player.currentTime());
+    });
+    // pause the video in 23.4s
+    player.on("timeupdate", () => {
+      // console.log("player is timeupdate");
+      // show the certain time
+      // console.log(player.currentTime());
+      // set a array index to get the video_interrupt_time
+      console.log(info[arrayNum].video_interrupt_time);
+      console.log(arrayNum);
+      if (arrayNum < info.length) {
+        if (player.currentTime() >= info[arrayNum].video_interrupt_time) {
+          player.pause();
+          // set the timer with the duration time after duration time, the video will resume
+          setTimeout(() => {
+            // player.load();
+            playerRef.current.play();
+          }, 2000);
+          arrayNum++;
+        }
+      }
+    });
+    player.on("resume", () => {
+      console.log("player is resume");
+      // show the certain time
+      console.log(player.currentTime());
     });
   };
 
