5ade0b10fa1866d08b59d88c89582eddf98da5b0
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-04-20 15:07:29 +0800

    新增路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Header from './managed/Header';
-import Footer from './managed/Footer';
-import UserLoginForm from './client/UserLogin';
-import BackendRegistration from './managed/Form/BackendRegistration';
-import Home from './managed/Pages/Home';
-import Pratice from './managed/Pages/Pratice';
-import Exam from './managed/Pages/Exam';
-import VideoPlayer from './managed/Pages/VideoPlayer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/app.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./managed/Header";
+import Footer from "./managed/Footer";
+import UserLoginForm from "./client/UserLogin";
+import BackendRegistration from "./managed/Form/BackendRegistration";
+import FrontEndRegistration from "./managed/Form/FrontEndRegistration";
+import Home from "./managed/Pages/Home";
+import Pratice from "./managed/Pages/Pratice";
+import Exam from "./managed/Pages/Exam";
+import VideoPlayer from "./managed/Pages/VideoPlayer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/app.css";
 
 function App() {
   return (
     // Routes 若有網址則如第一範例/Register前面須加上/#組合起來為/#/Register
     //  <Route exact path="/" element={<UserLoginForm />} />
-    <div className='container'>
+    <div className="container">
       <Header />
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Admin/Register' element={<BackendRegistration />} />
-          <Route path='/Pratice' element={<Pratice />} />
-          <Route path='/Exam' element={<Exam />} />
-          <Route path='/Video' element={<VideoPlayer />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Admin/Register" element={<BackendRegistration />} />
+          <Route path="/Client/Register" element={<FrontEndRegistration />} />
+          <Route path="/Pratice" element={<Pratice />} />
+          <Route path="/Exam" element={<Exam />} />
+          <Route path="/Video" element={<VideoPlayer />} />
         </Routes>
       </main>
       <Footer />
