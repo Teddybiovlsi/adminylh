8973c7838863882618cd37c1c791260c86ef4359
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-07-18 20:56:23 +0800

    預覽表單實作中

--- src/managed/Form/EditClientVideoQA.jsx
+++ src/managed/Form/EditClientVideoQA.jsx
@@ -11,7 +11,7 @@ import LoadingComponent from "../../components/LoadingComponent";
 import { get } from "../axios";
 
 export default function EditClientVideoQA({
-  FormMode = false,
+  FormMode = true,
   GoPrevEvent = null,
   GoNextEvent = null,
 }) {
@@ -20,6 +20,7 @@ export default function EditClientVideoQA({
   if (!location) {
     return <Navigate to="/" replace />;
   }
+  const [page, setPage] = useState(1);
 
   const videoLink = location?.state?.videoLink;
 
@@ -34,7 +35,11 @@ export default function EditClientVideoQA({
   const [tempVideoQA, setTempVideoQA] = useState([]);
 
   const [tempVideoInfo, setTempVideoInfo] = useState([]);
-
+  // 預覽表單頁面
+  const GoPriviewPage = () => {
+    setPage(page + 1);
+  };
+  // 取得先前資料
   const handlePrevData = () => {
     tempVideoInfo.forEach((info) => {
       if (info?.option_3[0] !== null && info?.option_4[0] !== null) {
@@ -253,71 +258,73 @@ export default function EditClientVideoQA({
   if (isLoading) {
     return <LoadingComponent title="台大分院雲林分院 編輯表單系統" />;
   }
+  switch (page) {
+    case 1:
+      return (
+        <div className="FormStyle d-flex align-items-center justify-content-center">
+          <PageTitle title={`台大分院雲林分院｜ 編輯表單系統`} />
+          <Card className={`${styles.ExamQusetionCard}`}>
+            <Card.Title className={styles.FormTitle} style={{ margin: 0 }}>
+              <CardTitleFunction TitleName={`台大醫院雲林分院`} />
+              <CardTitleFunction TitleName={`編輯表單系統`} />
+            </Card.Title>
 
-  return (
-    <div className="FormStyle d-flex align-items-center justify-content-center">
-      <PageTitle
-        title={`台大分院雲林分院｜ ${FormMode ? "測驗用表單" : "練習用表單"}`}
-      />
-      <Card className={`${styles.ExamQusetionCard}`}>
-        <Card.Title className={styles.FormTitle} style={{ margin: 0 }}>
-          <CardTitleFunction TitleName={`台大醫院雲林分院`} />
-          <CardTitleFunction
-            TitleName={`${FormMode ? "測驗用" : "練習用"}表單系統`}
-          />
-        </Card.Title>
+            <Card.Body className="pt-0 ps-0 pe-0">
+              <video ref={videoRef} src={videoLink} width="100%" controls />
+              <Stack direction="horizontal" className="ms-2 mt-3 mb-3 me-2">
+                <div>
+                  <h2>
+                    <strong>{`請填寫衛教${
+                      FormMode ? "測驗用" : "練習用"
+                    }影片問題`}</strong>
+                  </h2>
+                </div>
 
-        <Card.Body className="pt-0 ps-0 pe-0">
-          <video ref={videoRef} src={videoLink} width="100%" controls />
-          <Stack direction="horizontal" className="ms-2 mt-3 mb-3 me-2">
-            <div>
-              <h2>
-                <strong>{`請填寫衛教${
-                  FormMode ? "測驗用" : "練習用"
-                }影片問題`}</strong>
-              </h2>
-            </div>
+                <div className="ms-auto">
+                  <BtnBootstrap
+                    text={"新增問題"}
+                    onClickEventName={handleAddQuestion}
+                    variant="secondary"
+                  />
+                </div>
+              </Stack>
 
-            <div className="ms-auto">
-              <BtnBootstrap
-                text={"新增問題"}
-                onClickEventName={handleAddQuestion}
-                variant="secondary"
+              <DynamicQuestionandAnswer
+                FormMode={FormMode}
+                VideoQA={tempVideoQA}
+                handleDelQAMessage={handleDelQAMessage}
+                handleGetVideoTime={handleGetVideoTime}
+                handleGetVideoDuration={handleGetVideoDuration}
+                handleGetQuestionMustCorrect={handleGetQuestionMustCorrect}
+                handleGetQuestionContent={handleGetQuestionContent}
+                handleOptionChange={handleOptionChange}
+                handleIsCorrectOption={handleIsCorrectOption}
+                handleAnswerChange={handleAnswerChange}
               />
-            </div>
-          </Stack>
-
-          <DynamicQuestionandAnswer
-            FormMode={FormMode}
-            VideoQA={tempVideoQA}
-            handleDelQAMessage={handleDelQAMessage}
-            handleGetVideoTime={handleGetVideoTime}
-            handleGetVideoDuration={handleGetVideoDuration}
-            handleGetQuestionMustCorrect={handleGetQuestionMustCorrect}
-            handleGetQuestionContent={handleGetQuestionContent}
-            handleOptionChange={handleOptionChange}
-            handleIsCorrectOption={handleIsCorrectOption}
-            handleAnswerChange={handleAnswerChange}
-          />
-        </Card.Body>
-        <Card.Footer>
-          <BtnBootstrap
-            btnPosition="ms-2  float-end"
-            btnName={"formStep"}
-            text={"預覽表單"}
-            onClickEventName={GoPrevEvent}
-            variant="primary"
-            disabled={ifBtnDisable}
-          />
-          <BtnBootstrap
-            btnPosition="ms-2  float-end"
-            btnName={"formStep"}
-            text={"驗證此頁面表單"}
-            onClickEventName={validateQA}
-            variant="success"
-          />
-        </Card.Footer>
-      </Card>
-    </div>
-  );
+            </Card.Body>
+            <Card.Footer>
+              <BtnBootstrap
+                btnPosition="ms-2  float-end"
+                btnName={"formStep"}
+                text={"預覽表單"}
+                onClickEventName={GoPriviewPage}
+                variant="primary"
+                disabled={ifBtnDisable}
+              />
+              <BtnBootstrap
+                btnPosition="ms-2  float-end"
+                btnName={"formStep"}
+                text={"驗證此頁面表單"}
+                onClickEventName={validateQA}
+                variant="success"
+              />
+            </Card.Footer>
+          </Card>
+        </div>
+      );
+    case 2:
+      return <div>預覽表單</div>;
+    default:
+      return <Navigate to="/" replace />;
+  }
 }
