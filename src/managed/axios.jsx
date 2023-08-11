1645ebdf242b330690cec6eee2957bf1630febf9
Teddybiovlsi <117162287+Teddybiovlsi@users.noreply.github.com>
2023-08-11 16:37:06 +0800

    修改API路徑

--- src/managed/axios.jsx
+++ src/managed/axios.jsx
@@ -3,7 +3,7 @@ import _axios from "axios";
 const axios = (baseURL, passType = "multipart/form-data") => {
   // 建立自訂義的axios
   const instance = _axios.create({
-    baseURL: baseURL || "http://140.125.35.82:8079/ntuh_laravel_API/public/", //JSON-Server端口位置
+    baseURL: baseURL || "http://140.125.35.8:8079/ntuh_laravel_API/public/", //JSON-Server端口位置
     headers: { "Content-Type": passType, charset: "utf-8" },
     timeout: 5000,
   });
@@ -18,7 +18,7 @@ export const post = (path, data) => axios().post(`api/v1/POST/${path}`, data);
 // make axios put request api from the baseURL and /api/v1/PUT/ + path
 export const put = (path, data) =>
   axios(
-    "http://140.125.35.82:8079/ntuh_laravel_API/public/",
+    "http://140.125.35.8:8079/ntuh_laravel_API/public/",
     "application/x-www-form-urlencoded"
   ).put(`api/v1/PUT/${path}`, data);
 // make axios delete request api from the baseURL and /api/v1/DELETE/ + path
