359a8015b0dc1cf654adcc5f2ce0572529cda20a
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-09-20 14:19:50 +0800

    新增篩選資料筆數分頁

--- src/managed/Pages/ManageClientRecord.jsx
+++ src/managed/Pages/ManageClientRecord.jsx
@@ -1,8 +1,10 @@
+import limitPage from "../JsonFile/FilterPageContentSize.json";
 import React, { useEffect, useState } from "react";
-import { Accordion, Container, Row, Col } from "react-bootstrap";
+import { Accordion, Container, Row, Col, Form } from "react-bootstrap";
 import PageTitle from "../../components/Title";
 import PageTitleHeading from "../../components/PageTitleHeading";
 import { get } from "../axios";
+import ReactPaginate from "react-paginate";
 
 export default function ManageClientRecord() {
   const convertType = (type) => {
@@ -17,9 +19,24 @@ export default function ManageClientRecord() {
   };
 
   const [loading, setLoading] = useState(true);
-
+  //   原始使用者紀錄資料
   const [eachUserRecord, setEachUserRecord] = useState([]);
+  //   slice後的使用者紀錄資料
+  const [eachUserRecordSlice, setEachUserRecordSlice] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
+
+  const [paginationSettings, setPaginationSettings] = useState({
+    rowsPerPage: 5,
+    currentPage: 0,
+    lastPage: 1,
+  });
+
+  const handlePageChange = (page) => {
+    setPaginationSettings({
+      ...paginationSettings,
+      currentPage: page,
+    });
+  };
 
   const fetchaAccountData = async ({ api }) => {
     try {
@@ -28,6 +45,7 @@ export default function ManageClientRecord() {
       const checkIsArray = Array.isArray(data);
 
       setEachUserRecord(checkIsArray ? data : [data]);
+      setEachUserRecordSlice(checkIsArray ? data : [data]);
       // 將 loading 設為 false
       setLoading(false);
       // clear error message
@@ -57,13 +75,57 @@ export default function ManageClientRecord() {
     };
   }, []);
 
+  useEffect(() => {
+    const start =
+      paginationSettings.currentPage * paginationSettings.rowsPerPage;
+    const end = start + paginationSettings.rowsPerPage;
+    setEachUserRecordSlice(eachUserRecord.slice(start, end));
+    setPaginationSettings({
+      ...paginationSettings,
+      lastPage: Math.ceil(
+        eachUserRecord.length / paginationSettings.rowsPerPage
+      ),
+    });
+  }, [
+    eachUserRecord,
+    paginationSettings.currentPage,
+    paginationSettings.rowsPerPage,
+  ]);
+
   return (
     <>
       <PageTitle title="臺大醫院雲林分院-衛教系統 使用者紀錄管理" />
       <PageTitleHeading text={"紀錄資訊欄位"} styleOptions={3} />
       <Container>
-        <Accordion>
-          {eachUserRecord.map((user, index) => {
+        <Row>
+          <Col md={{ span: 4, offset: 8 }}>
+            <Form.Select
+              className="mt-1"
+              aria-label="請選擇每頁顯示筆數"
+              onChange={(event) => {
+                setPaginationSettings({
+                  ...paginationSettings,
+                  currentPage: 0,
+                  rowsPerPage: Number(event.target.value),
+                });
+              }}
+              selected={paginationSettings.rowsPerPage}
+            >
+              {
+                // use map to show 每頁顯示筆數
+                limitPage.map((limit, _) => {
+                  return (
+                    <option key={limit.id} value={limit.value}>
+                      每頁顯示{limit.value}筆
+                    </option>
+                  );
+                })
+              }
+            </Form.Select>
+          </Col>
+        </Row>
+        <Accordion className="mb-3">
+          {eachUserRecordSlice.map((user, index) => {
             return (
               <Accordion.Item eventKey={user.name} key={user.name}>
                 <Accordion.Header>{user.name}</Accordion.Header>
@@ -149,6 +211,29 @@ export default function ManageClientRecord() {
             );
           })}
         </Accordion>
+        <ReactPaginate
+          forcePage={paginationSettings.currentPage}
+          breakLabel={"..."}
+          previousLabel={"<"}
+          nextLabel={">"}
+          onPageChange={(page) => {
+            handlePageChange(page.selected);
+          }}
+          pageRangeDisplayed={2}
+          marginPagesDisplayed={1}
+          pageCount={paginationSettings.lastPage}
+          renderOnZeroPageCount={null}
+          containerClassName="justify-content-center pagination"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          activeClassName="active"
+        />
       </Container>
     </>
   );
