7afcae0725e5fd14d823bd431318df7d19f3d95e
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-09-21 12:00:56 +0800

    Rewrite fetchaLoginData function using useCallback hook

--- src/managed/Pages/LogIn.jsx
+++ src/managed/Pages/LogIn.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { Container, Form, Col, Row } from "react-bootstrap";
-import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { post } from "../axios";
 import BtnBootstrap from "../../components/BtnBootstrap";
@@ -9,10 +8,8 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function LogIn() {
-  const [checkuserInfo, setCheckuserInfo] = useState(
-    !localStorage.getItem("manage") && !sessionStorage.getItem("manage")
-  );
-
+  const isUserLoggedIn =
+    !localStorage.getItem("manage") && !sessionStorage.getItem("manage");
   const [userInfo, setUserInfo] = useState({
     account: "",
     password: "",
@@ -20,34 +17,32 @@ export default function LogIn() {
   });
 
   const [tempuser, setTempUser] = useState(null);
-  let navigate = useNavigate();
+  const navigate = useNavigate();
   const [validated, setValidated] = useState(false);
   const [ErrorMessage, setErrorMessage] = useState("");
+  const [isDisabledLoginButton, setIsDisabledLoginButton] = useState(false);
 
-  useEffect(() => {
-    if (!checkuserInfo) {
-      navigate("/Home");
-    }
-  }, [checkuserInfo]);
+  const handleSubmit = useCallback(
+    (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      if (form.checkValidity() === true) {
+        event.preventDefault();
+        fetchaLoginData(userInfo);
+      }
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    if (form.checkValidity() === true) {
-      event.preventDefault();
-      fetchaLoginData(userInfo);
-    }
+      setValidated(true);
+    },
+    [userInfo]
+  );
 
-    setValidated(true);
-  };
-
-  const fetchaLoginData = async (data) => {
-    let clientSubmit = toast.loading("登入中...");
+  const fetchaLoginData = useCallback(async (data) => {
+    const clientSubmit = toast.loading("登入中...");
+    setIsDisabledLoginButton(true);
     try {
-      console.log(data);
       const response = await post("admin/login", data);
 
       const userInfo = await response.data;
@@ -61,13 +56,11 @@ export default function LogIn() {
         isLoading: false,
         autoClose: 3000,
       });
-
       setTimeout(() => {
-        navigate("/Home");
+        setIsDisabledLoginButton(false);
       }, 3000);
     } catch (error) {
       // console.log(error.response.data);
-      console.log(error.code);
       if (error.code === "ECONNABORTED") {
         toast.update(clientSubmit, {
           render: "連線逾時，請稍後再試",
@@ -83,86 +76,100 @@ export default function LogIn() {
           autoClose: 3000,
         });
       }
+      setTimeout(() => {
+        setIsDisabledLoginButton(false);
+      }, 3000);
     }
-  };
+  }, []);
 
   useEffect(() => {
     if (tempuser !== null) {
       if (userInfo.isRemember) {
         localStorage.setItem("manage", JSON.stringify(tempuser));
-        setCheckuserInfo(tempuser);
+        setTimeout(() => {
+          navigate("/Home");
+        }, 3000);
       } else {
         sessionStorage.setItem("manage", JSON.stringify(tempuser));
-        setCheckuserInfo(tempuser);
+        setTimeout(() => {
+          navigate("/Home");
+        }, 3000);
       }
     }
-  }, [tempuser]);
+  }, [tempuser, userInfo.isRemember]);
 
-  if (!localStorage.getItem("manage") || !sessionStorage.getItem("manage")) {
-    return (
-      <Container>
-        <h1 className="text-center">歡迎光臨台大衛教系統</h1>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Col>
-            <Form.Group as={Row} md="4" controlId="validationCustom01">
-              <Form.Label>帳號</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="請輸入帳號"
-                onChange={(e) => {
-                  setUserInfo({ ...userInfo, account: e.target.value });
+  useEffect(() => {
+    if (!isUserLoggedIn) {
+      navigate("/Home");
+    }
+  }, [isUserLoggedIn, navigate]);
+
+  // const clientSubmit = useMemo(() => toast.loading("登入中..."), []);
+
+  return (
+    <Container>
+      <h1 className="text-center">歡迎光臨台大衛教系統</h1>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Col>
+          <Form.Group as={Row} md="4" controlId="validationCustom01">
+            <Form.Label>帳號</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="請輸入帳號"
+              onChange={(e) => {
+                setUserInfo({ ...userInfo, account: e.target.value });
+              }}
+            />
+            <Form.Control.Feedback type="invalid">
+              請輸入帳號
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Row} controlId="formPwd">
+            <Form.Label>密碼</Form.Label>
+            <Form.Control
+              required
+              type="password"
+              placeholder="請輸入密碼"
+              onChange={(e) => {
+                setUserInfo({ ...userInfo, password: e.target.value });
+              }}
+              isInvalid={ErrorMessage.passwordErrorMessage}
+            />
+            <Form.Control.Feedback type="invalid">
+              請輸入密碼
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Row>
+            <Col>
+              <Form.Check
+                type="checkbox"
+                label="記住我"
+                className="mt-2"
+                id="remember"
+                value={userInfo.isRemember}
+                onClick={() => {
+                  setUserInfo({
+                    ...userInfo,
+                    isRemember: !userInfo.isRemember,
+                  });
                 }}
               />
-              <Form.Control.Feedback type="invalid">
-                請輸入帳號
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Row} controlId="formPwd">
-              <Form.Label>密碼</Form.Label>
-              <Form.Control
-                required
-                type="password"
-                placeholder="請輸入密碼"
-                onChange={(e) => {
-                  setUserInfo({ ...userInfo, password: e.target.value });
-                }}
-                isInvalid={ErrorMessage.passwordErrorMessage}
+            </Col>
+            <Col>
+              <BtnBootstrap
+                btnPosition="mt-2 float-end"
+                btnSize="md"
+                variant="primary"
+                btnType="submit"
+                text="登入"
+                disabled={isDisabledLoginButton}
               />
-              <Form.Control.Feedback type="invalid">
-                請輸入密碼
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Row>
-              <Col>
-                <Form.Check
-                  type="checkbox"
-                  label="記住我"
-                  className="mt-2"
-                  id="remember"
-                  value={userInfo.isRemember}
-                  onClick={() => {
-                    setUserInfo({
-                      ...userInfo,
-                      isRemember: !userInfo.isRemember,
-                    });
-                  }}
-                />
-              </Col>
-              <Col>
-                <BtnBootstrap
-                  btnPosition="mt-2 float-end"
-                  btnSize="md"
-                  variant="primary"
-                  btnType="submit"
-                  text="登入"
-                />
-              </Col>
-            </Row>
-          </Col>
-        </Form>
-        <ToastAlert />
-      </Container>
-    );
-  }
+            </Col>
+          </Row>
+        </Col>
+      </Form>
+      <ToastAlert />
+    </Container>
+  );
 }
