e9cb59eadb61593a678e9c8a596685cefd2cedae
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-09-26 21:15:18 +0800

    將程式必要之元件重新排序

--- src/managed/Pages/Exam.jsx
+++ src/managed/Pages/Exam.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import CreateVideo from "../Form/CreateVideo";
 
 export default function Exam() {
-  return (
-    <>
-      <CreateVideo VideoMode={true} />
-    </>
-  );
+  return <CreateVideo VideoMode={true} />;
 }
--- src/managed/Pages/Pratice.jsx
+++ src/managed/Pages/Pratice.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import CreateVideo from "../Form/CreateVideo";
 
 export default function Pratice() {
-  return <CreateVideo />;
+  return <CreateVideo VideoMode={false} />;
 }
