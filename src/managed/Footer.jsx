357775932b5d3d42a4e9f5acfc0cf70b61a89b12
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-09-26 16:39:39 +0800

    移除捨棄之元件

--- src/managed/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/components/FooterStyle.module.scss";
-
-export default function Footer() {
-  return (
-    <footer id={styles.footerStyle}>
-      <small>&copy; Dr H Group, 2023. All rights reserved.</small>
-    </footer>
-  );
-}
