7f875f2a584b18752e0a4d3795ca0b2f376b6d63
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-09-26 21:11:51 +0800

    路由保護新增清除瀏覽器暫存

--- src/AuthProtected.jsx
+++ src/AuthProtected.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 
 export default function AuthProtected({ user, redirectPath = "/", children }) {
   // const user = JSON.parse(localStorage?.getItem("user"));
   const nowTime = new Date();
 
   if (!user) {
+    localStorage.getItem("manage") && localStorage.clear();
+    sessionStorage.getItem("manage") && sessionStorage.clear();
     return <Navigate to="/" />;
   } else {
     if (new Date(user.expTime) < nowTime) {
-      // console.log("憑證過期");
-      localStorage.removeItem("user");
+      localStorage.getItem("manage") && localStorage.clear();
+      sessionStorage.getItem("manage") && sessionStorage.clear();
       return <Navigate to={redirectPath} />;
     } else {
       // console.log("憑證未過期", user.expTime, nowTime);
