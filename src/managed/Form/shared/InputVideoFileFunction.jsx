22cccf79c3fa09cb92954715e32129aab1e9ac9f
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-10-18 14:21:31 +0800

    修改版面樣式，新增部分註解

--- src/managed/Form/shared/InputVideoFileFunction.jsx
+++ src/managed/Form/shared/InputVideoFileFunction.jsx
@@ -1,62 +1,58 @@
 import React from "react";
-import { Form, Card } from "react-bootstrap";
-import PageTitle from "../../../components/Title";
-import { CardTitleFunction } from "./CardTitleFunction";
+import { Form, Container, Row } from "react-bootstrap";
 import BtnBootstrap from "../../../components/BtnBootstrap";
 import styles from "../../../styles/Form/FormStyles.module.scss";
 
 function InputVideoFileFunction({
-  FormMode = false,
-  ChangeEvent = null,
+  // 輸入影片文件的相關變數
   VidoeName = "",
+  ChangeEvent = null,
+
+  // 表單模式和下一步事件的相關變數
+  FormMode = false,
   GoNextEvent = null,
 }) {
-  return (
-    <>
-      <PageTitle
-        title={`台大分院雲林分院｜ ${FormMode ? "測驗用表單" : "練習用表單"}`}
-      />
-      <div className="FormStyle d-flex align-items-center justify-content-center">
-        <Card className={`${styles.ExamVideoInputCard}`}>
-          <Card.Title className={`${styles.FormTitle}`}>
-            <CardTitleFunction TitleName={`台大醫院雲林分院`} />
-            <CardTitleFunction
-              TitleName={`${FormMode ? "測驗用" : "練習用"}表單系統`}
-            />
-          </Card.Title>
-          <Card.Body>
-            <Form.Group controlId="formFile">
-              <Form.Label>
-                <h2>
-                  <strong>
-                    請匯入衛教{FormMode ? "測驗用" : "練習用"}影片
-                  </strong>
-                </h2>
-              </Form.Label>
+  const title = `請匯入衛教${FormMode ? "測驗用" : "練習用"}影片`;
 
-              <Form.Control
-                type="file"
-                name="videoFileInput"
-                accept="video/*"
-                onChange={ChangeEvent}
-              />
-              <Form.Label className={`mt-3 ${styles.ExamVideoFileNameContainer}`}>
-                <h5>{VidoeName && <strong>目前檔案為：<br /> {VidoeName}</strong>}</h5>
-              </Form.Label>
-            </Form.Group>
-          </Card.Body>
-          <Card.Footer>
-            <BtnBootstrap
-              btnName="formStep"
-              variant="primary"
-              text={"下一步"}
-              onClickEventName={GoNextEvent}
-              disabled={!VidoeName}
-            />
-          </Card.Footer>
-        </Card>
-      </div>
-    </>
+  return (
+    <Container>
+      <Row>
+        <Form.Group controlId="formFile">
+          <Form.Label>
+            <h2>
+              <strong>{title}</strong>
+            </h2>
+          </Form.Label>
+
+          <Form.Control
+            accept="video/*"
+            name="videoFileInput"
+            onChange={ChangeEvent}
+            type="file"
+          />
+          <Form.Label className={`mt-3 ${styles.ExamVideoFileNameContainer}`}>
+            <h5>
+              {VidoeName && (
+                <strong>
+                  目前檔案為：
+                  <br /> {VidoeName}
+                </strong>
+              )}
+            </h5>
+          </Form.Label>
+        </Form.Group>
+      </Row>
+      <Row>
+        <BtnBootstrap
+          btnName="formStep"
+          variant="outline-primary"
+          btnSize="btn-md"
+          text={"下一步"}
+          onClickEventName={GoNextEvent}
+          disabled={!VidoeName}
+        />
+      </Row>
+    </Container>
   );
 }
 
