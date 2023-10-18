392698e4e4ad0a56c768fe9e9c5a76a0f7d64fe9
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-10-18 15:34:42 +0800

    刪減不必要之引入，優化程式碼

--- src/managed/Form/shared/InputVideoLanguageFunction.jsx
+++ src/managed/Form/shared/InputVideoLanguageFunction.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import PageTitle from "../../../components/Title";
-import { Form, Card, FloatingLabel } from "react-bootstrap";
+import {
+  Form,
+  Card,
+  FloatingLabel,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 import {
   CardTitleFunction,
   CardSecondTitleFunction,
@@ -8,74 +15,77 @@ import {
 import BtnBootstrap from "../../../components/BtnBootstrap";
 import styles from "../../../styles/Form/FormStyles.module.scss";
 
-function InputVideoInfoFunction({
-  FormMode = false,
-  ChangeEvent = null,
+function InputVideoLanguageFunction({
+  // 選擇影片語言的變數
   VideoLanguage = 0,
+
+  // 表單模式和下一步事件的相關變數
+  ChangeEvent = null,
   GoPrevEvent = null,
   GoNextEvent = null,
 }) {
+  const VideoTitleLanguage = `請點選衛教影片語言`;
+
+  const languageOptions = [
+    { value: 1, label: "國語" },
+    { value: 2, label: "台語" },
+    { value: 3, label: "英文" },
+    { value: 4, label: "日文" },
+    { value: 5, label: "越南文" },
+    { value: 6, label: "泰文" },
+    { value: 7, label: "印尼語" },
+    { value: 8, label: "菲律賓語" },
+  ];
+
   return (
-    <div className="FormStyle d-flex align-items-center justify-content-center">
-      <PageTitle
-        title={`台大分院雲林分院｜ ${FormMode ? "測驗用表單" : "練習用表單"}`}
-      />
-      <Card className={`${styles.ExamVideoInfoCard}`}>
-        <Card.Title className={`${styles.FormTitle}`}>
-          <CardTitleFunction TitleName={`台大醫院雲林分院`} />
-          <CardTitleFunction
-            TitleName={`${FormMode ? "測驗用" : "練習用"}表單系統`}
-          />
-        </Card.Title>
-        <Card.Body>
-          <Form.Label>
-            <CardSecondTitleFunction
-              TitleName={`請點選衛教${FormMode ? "測驗用" : "練習用"}影片語言`}
-            />
-          </Form.Label>
-          <FloatingLabel controlId="floatingSelect" label="請選擇影片語言">
-            <Form.Select
-              name="videoFileLanguage"
-              aria-label="Default select example"
-              size="lg"
-              onChange={ChangeEvent}
-              defaultValue={VideoLanguage}
-              style={{
-                paddingBottom: 0,
-              }}
-            >
-              <option value={""}>請點擊開啟語言選單</option>
-              <option value={1}>國語</option>
-              <option value={2}>台語</option>
-              <option value={3}>英文</option>
-              <option value={4}>日文</option>
-              <option value={5}>越南文</option>
-              <option value={6}>泰文</option>
-              <option value={7}>印尼語</option>
-              <option value={8}>菲律賓語</option>
-            </Form.Select>
-          </FloatingLabel>
-        </Card.Body>
-        <Card.Footer>
+    <Container>
+      <Row>
+        <Form.Label>
+          <h2>
+            <strong>{VideoTitleLanguage}</strong>
+          </h2>
+        </Form.Label>
+
+        <Form.Select
+          aria-label="請選擇影片的語言"
+          defaultValue={VideoLanguage}
+          name="videoFileLanguage"
+          onChange={ChangeEvent}
+          size="lg"
+        >
+          <option value={""}>請點擊開啟語言選單</option>
+          {languageOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </Form.Select>
+      </Row>
+      <Row className="mt-4">
+        <Col>
           <BtnBootstrap
-            btnPosition="ms-2"
+            btnPosition=""
+            btnSize="btn-md"
             btnName="formStep"
-            variant="danger"
+            variant="outline-danger"
             text={"上一步"}
             onClickEventName={GoPrevEvent}
           />
+        </Col>
+        <Col xs={5}>
           <BtnBootstrap
-            btnPosition="ms-2 float-end"
+            btnPosition="float-end"
+            btnSize="btn-md"
             btnName="formStep"
-            variant="primary"
+            variant="outline-primary"
             text={"下一步"}
             onClickEventName={GoNextEvent}
             disabled={VideoLanguage ? false : true}
           />
-        </Card.Footer>
-      </Card>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-export default InputVideoInfoFunction;
+export default InputVideoLanguageFunction;
