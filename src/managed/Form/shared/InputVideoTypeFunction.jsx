70effd2902f05bab0f73f4a2a1b9b69d05215a7b
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-10-18 15:47:51 +0800

    修改版面

--- src/managed/Form/shared/InputVideoTypeFunction.jsx
+++ src/managed/Form/shared/InputVideoTypeFunction.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import PageTitle from "../../../components/Title";
-import { Form, Card, FloatingLabel } from "react-bootstrap";
+import {
+  Form,
+  Card,
+  FloatingLabel,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 import {
   CardTitleFunction,
   CardSecondTitleFunction,
@@ -15,65 +22,63 @@ function InputVideoTypeFunction({
   GoPrevEvent = null,
   GoNextEvent = null,
 }) {
+  const TypeOptions = [
+    { value: 1, label: "疾病照護" },
+    { value: 2, label: "活動" },
+    { value: 3, label: "進食" },
+    { value: 4, label: "管路照護及異常處理" },
+    { value: 5, label: "皮膚照護" },
+    { value: 6, label: "傷口照護" },
+    { value: 7, label: "預防合併症" },
+  ];
+
   return (
-    <div className="FormStyle d-flex align-items-center justify-content-center">
-      <PageTitle
-        title={`台大分院雲林分院｜ ${FormMode ? "測驗用表單" : "練習用表單"}`}
-      />
-      <Card className={`${styles.ExamVideoTypeCard}`}>
-        <Card.Title className={`${styles.FormTitle}`}>
-          <CardTitleFunction TitleName={`台大醫院雲林分院`} />
-          <CardTitleFunction
-            TitleName={`${FormMode ? "測驗用" : "練習用"}表單系統`}
-          />
-        </Card.Title>
-        <Card.Body>
-          <Form.Label>
-            <CardSecondTitleFunction
-              TitleName={`請點選衛教${FormMode ? "測驗用" : "練習用"}影片類別`}
-            />
-          </Form.Label>
-          <FloatingLabel controlId="floatingSelect" label="請選擇影片類別">
-            <Form.Select
-              name="videoFileLanguage"
-              aria-label="Default select example"
-              size="lg"
-              onChange={ChangeEvent}
-              defaultValue={VideoType}
-              style={{
-                paddingBottom: 0,
-              }}
-            >
-              <option value={""}>請點擊開啟類別選單</option>
-              <option value={1}>疾病照護</option>
-              <option value={2}>活動</option>
-              <option value={3}>進食</option>
-              <option value={4}>管路照護及異常處理</option>
-              <option value={5}>皮膚照護</option>
-              <option value={6}>傷口照護</option>
-              <option value={7}>預防合併症</option>
-            </Form.Select>
-          </FloatingLabel>
-        </Card.Body>
-        <Card.Footer>
+    <Container>
+      <Row>
+        <Form.Label>
+          <h2>
+            <strong>請點選衛教影片類別</strong>
+          </h2>
+        </Form.Label>
+        <Form.Select
+          aria-label="請選擇影片類別"
+          defaultValue={VideoType}
+          name="videoFileType"
+          onChange={ChangeEvent}
+          size="lg"
+        >
+          <option value={""}>請點擊開啟類別選單</option>
+          {TypeOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </Form.Select>
+      </Row>
+      <Row className="mt-4">
+        <Col>
           <BtnBootstrap
-            btnPosition="ms-2"
+            btnPosition=""
+            btnSize="btn-md"
             btnName="formStep"
-            variant="danger"
+            variant="outline-danger"
             text={"上一步"}
             onClickEventName={GoPrevEvent}
           />
+        </Col>
+        <Col xs={5}>
           <BtnBootstrap
-            btnPosition="ms-2 float-end"
+            btnPosition="float-end"
+            btnSize="btn-md"
             btnName="formStep"
-            variant="primary"
+            variant="outline-primary"
             text={"下一步"}
             onClickEventName={GoNextEvent}
             disabled={VideoType ? false : true}
           />
-        </Card.Footer>
-      </Card>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
