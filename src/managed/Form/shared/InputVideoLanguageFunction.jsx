464855ad812ffac8942a1c574f89f8b6116c296c
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2023-11-27 23:46:43 +0800

    Fix import typo in InputVideoLanguageFunction.jsx

--- src/managed/Form/shared/InputVideoLanguageFunction.jsx
+++ src/managed/Form/shared/InputVideoLanguageFunction.jsx
@@ -8,11 +8,7 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
-import {
-  CardTitleFunction,
-  CardSecondTitleFunction,
-} from "./CardTitleFunction";
-import BtnBootstrap from "../../../components/BtnBootstrap";
+import BtnBootstrap from "../../../components/BtnBootStrap";
 import styles from "../../../styles/Form/FormStyles.module.scss";
 
 function InputVideoLanguageFunction({
