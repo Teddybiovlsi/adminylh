05424e61ff19b136de26b79838e3f6f80193e674
Teddybiovlsi <M11113005@yuntech.edu.tw>
2023-12-23 21:16:07 +0800

    Update formType properties and activeStep

--- src/managed/Form/CreateBasicVideo.jsx
+++ src/managed/Form/CreateBasicVideo.jsx
@@ -428,8 +428,9 @@ export default function CreateBasicVideo() {
                 setFormType({
                   ...formType,
                   isSkipped: true,
+                  activeStep: 6,
                   completedSteps: formType.completedSteps.map((step, index) =>
-                    index === 5 ? true : step
+                    index === 5 ? false : step
                   ),
                   formStep: 6,
                 });
@@ -446,6 +447,7 @@ export default function CreateBasicVideo() {
                   completedSteps: formType.completedSteps.map((step, index) =>
                     index === 4 ? false : step
                   ),
+                  activeStep: 5,
                   formStep: formType.formStep + 1,
                 });
                 handleCloseConfirmSkipQuestion();
