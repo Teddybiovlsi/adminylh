b122599232fa070e74d6c3c088f3f065a77d9673
TeddyBiovlsi <m11113005@yuntech.edu.tw>
2024-03-19 16:01:11 +0800

    修正上傳按鍵以動畫spinner取代

--- src/managed/Form/shared/InputFormBasicPreviewFunction.jsx
+++ src/managed/Form/shared/InputFormBasicPreviewFunction.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { Card, Col, Container, Image, Row, Stack } from 'react-bootstrap';
-import BtnBootstrap from '../../../components/BtnBootstrap';
+import React, { useState } from "react";
+import { Card, Col, Container, Image, Row, Stack } from "react-bootstrap";
+import BtnBootstrap from "../../../components/BtnBootstrap";
+import BtnBootstrapwithSpinner from "../../../components/BtnBootstrapwithSpinner";
 import {
   SwitchNumToLanguage,
   SwitchNumToType,
-} from './func/SwitchNumToLanguage';
+} from "./func/SwitchNumToLanguage";
 
 export default function InputFormBasicPreviewFunction({
   isSkip = false,
   isCheckValidationQA = false,
-  VideoName = '',
-  VideoTitle = '',
-  VideoLanguage = '',
-  VideoType = '',
+  VideoName = "",
+  VideoTitle = "",
+  VideoLanguage = "",
+  VideoType = "",
   VideoQA,
   GoPrevEvent = null,
   SubmitEvent = null,
@@ -21,41 +22,41 @@ export default function InputFormBasicPreviewFunction({
   return (
     <Container>
       <Row>
-        <Col className='h5 ps-0' md={4}>
+        <Col className="h5 ps-0" md={4}>
           影片名稱:
         </Col>
-        <Col md={6}>{VideoTitle != '' ? VideoTitle : VideoName}</Col>
+        <Col md={6}>{VideoTitle != "" ? VideoTitle : VideoName}</Col>
       </Row>
 
       <Row>
-        <Col className='h5 ps-0' md={4}>
+        <Col className="h5 ps-0" md={4}>
           影片語言:
         </Col>
         <Col md={6}>{SwitchNumToLanguage(parseInt(VideoLanguage))}</Col>
       </Row>
 
       <Row>
-        <Col className='h5 ps-0' md={4}>
+        <Col className="h5 ps-0" md={4}>
           影片類型:
         </Col>
         <Col md={6}>{SwitchNumToType(parseInt(VideoType))}</Col>
       </Row>
       <Row>
-        <Col className='h5 ps-0' md={4}>
+        <Col className="h5 ps-0" md={4}>
           是否跳過填寫影片問題:
         </Col>
         <Col md={6}>
-          <b>{isSkip ? '是' : '否'}</b>{' '}
+          <b>{isSkip ? "是" : "否"}</b>{" "}
         </Col>
       </Row>
       {!isSkip &&
         isCheckValidationQA &&
         VideoQA?.map((q, i) => (
-          <Card key={i} className='mb-2'>
-            <Card.Title className='mb-2 ms-1'>問題 {i + 1}:</Card.Title>
+          <Card key={i} className="mb-2">
+            <Card.Title className="mb-2 ms-1">問題 {i + 1}:</Card.Title>
             <Container>
               <Row>
-                <Col className='h5 ps-0 ms-2' md={4}>
+                <Col className="h5 ps-0 ms-2" md={4}>
                   問題內容:
                 </Col>
                 <Col md={6}>{q.questionContent}</Col>
@@ -68,18 +69,18 @@ export default function InputFormBasicPreviewFunction({
 
             {q.answerContent.map((a, j) => (
               <div key={`${i}-${j}`}>
-                <Card.Title className='ms-2'>{`答案${String.fromCharCode(
+                <Card.Title className="ms-2">{`答案${String.fromCharCode(
                   65 + j
                 )}:`}</Card.Title>
                 <Card.Text
-                  className={`ms-4 ${a[0] ? 'text-success' : 'text-danger'}`}
-                >{`${a[1]}-答案為${a[0] ? '正確' : '錯誤'}`}</Card.Text>
+                  className={`ms-4 ${a[0] ? "text-success" : "text-danger"}`}
+                >{`${a[1]}-答案為${a[0] ? "正確" : "錯誤"}`}</Card.Text>
                 {q.answerFile[j] && (
                   <Card.Img
-                    variant='top'
+                    variant="top"
                     src={URL.createObjectURL(q.answerFile[j])}
                     alt={`optionImage${j}`}
-                    style={{ width: '100px' }}
+                    style={{ width: "100px" }}
                   />
                 )}
               </div>
@@ -87,23 +88,23 @@ export default function InputFormBasicPreviewFunction({
           </Card>
         ))}
 
-      <Stack gap={2} className='col-md-5 mx-auto'>
-        <BtnBootstrap
-          btnPosition=''
-          btnName='formStep'
-          btnSize='md'
-          disabled={SubmitEventDisabled}
-          text={'送出表單'}
+      <Stack gap={2} className="col-md-5 mx-auto">
+        <BtnBootstrapwithSpinner
+          btnName="formStep"
+          btnSize="md"
+          text={"送出表單"}
+          loadingText="送出中..."
           onClickEventName={SubmitEvent}
-          variant='outline-primary'
+          ifIsLoading={SubmitEventDisabled}
+          variant="outline-primary"
         />
         <BtnBootstrap
-          btnPosition=''
-          btnName={'formStep'}
-          btnSize='md'
-          text={'上一步'}
+          btnPosition=""
+          btnName={"formStep"}
+          btnSize="md"
+          text={"上一步"}
           onClickEventName={GoPrevEvent}
-          variant={'outline-danger'}
+          variant={"outline-danger"}
         />
       </Stack>
     </Container>
